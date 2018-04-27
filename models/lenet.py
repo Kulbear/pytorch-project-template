43ce54e23471e895cb5cfa97c262303b256b63b6
Kulbear <jiyang.py@gmail.com>
2018-04-27 12:06:19 -0600

    Add LeNet

---
+++ models/lenet.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.classifier = nn.Sequential(
+            nn.Linear(256, 120),
+            nn.ReLU(inplace=True),
+            nn.Linear(120, 84),
+            nn.ReLU(inplace=True),
+            nn.Linear(84, 10))
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(-1, self.num_flat_features(x))
+        x = self.classifier(x)
+        return F.log_softmax(x, dim=1)
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
